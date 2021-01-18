ae7923bbb6fd3347e7040add0b44f614e0221827
Joshua Galit <joshuaimalay@gmail.com>
2021-01-18 21:57:23 +0800

    fix the focus border default style in @tailwindcss/forms

--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,13 @@
           <div class="space-y-4">
             <div class="space-y-2">
               <label class="text-sm text-gray-600">Email / Slot Code</label>
-              <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+              <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                       placeholder="Enter Email" 
                       type="text" />
             </div>
             <div class="space-y-2">
               <label class="text-sm text-gray-600">Password</label>
-              <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+              <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                       placeholder="Enter Password" 
                       type="password" />
             </div>
--- pages/registration.vue
+++ pages/registration.vue
@@ -5,38 +5,38 @@
         <div class="space-y-4">
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Full Name *</label>
-            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                     placeholder="Enter Fullname" 
                     type="text" />
           </div>
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Gender *</label>
-            <select class="text-sm w-full py-2 px-3 border border-gray-300 rounded shadow-inner focus:outline-none focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 bg-white">
+            <select class="text-sm w-full py-2 px-3 border border-gray-300 rounded shadow-inner focus:outline-none focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 bg-white focus:border-gray-100">
               <option value="Male">Male</option>
               <option value="Male">Female</option>
             </select>
           </div>
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Country *</label>
-            <select class="text-sm w-full py-2 px-3 border border-gray-300 rounded shadow-inner focus:outline-none focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 bg-white">
+            <select class="text-sm w-full py-2 px-3 border border-gray-300 rounded shadow-inner focus:outline-none focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 bg-white focus:border-gray-100">
               <option>Philippines</option>
             </select>
           </div>
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Contact Number</label>
-            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                     placeholder="Enter Contact Number" 
                     type="password" />
           </div>
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Your Email * </label>
-            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                     placeholder="Enter Email" 
                     type="password" />
           </div>
           <div class="space-y-2">
             <label class="text-sm text-gray-600">Password *</label>
-            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200" 
+            <input  class="text-sm w-full py-2 border border-gray-300 rounded shadow-inner focus:outline-none pl-4 focus:ring-2 focus:ring-green-400 transition ease-in-out duration-200 focus:border-gray-100" 
                     placeholder="Enter Password" 
                     type="password" />
           </div>
