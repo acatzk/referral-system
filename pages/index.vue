fcd32a876e4d96320d6253f1c507f6547b22d526
Joshua Galit <joshuaimalay@gmail.com>
2021-01-18 19:33:04 +0800

    create nuxt-project & set-up tailwindcss config

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-semibold text-gray-400">hello Grace wellness</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Grace wellness'
+    }
+  }
+</script>
