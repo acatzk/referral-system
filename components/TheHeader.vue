affde504f45a7dd75c78bd4dc70407a47f00c704
Joshua Galit <joshuaimalay@gmail.com>
2021-01-19 13:45:11 +0800

    Header design - DONE

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>This is the header</h1>
+  <div class="flex items-center justify-between bg-green-500 py-1 px-6">
+    <div class="flex items-center space-x-6">
+      <button class="focus:outline-none rounded focus:ring-2 ring-green-400 p-1">
+        <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path></svg>
+      </button>
+      <div>
+        <img class="h-10" src="@/assets/images/xample-logo.png" alt="avatar" />
+      </div>
+    </div>
+    <div>
+      <button class="focus:outline-none rounded-full focus:ring-2 ring-green-400 p-1">
+        <svg class="w-6 h-6 fill-current text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.379 16.913A6.698 6.698 0 0119 11.788V9c0-3.519-2.614-6.432-6-6.92V1a1 1 0 10-2 0v1.08C7.613 2.568 5 5.481 5 9v2.788a6.705 6.705 0 01-2.388 5.133A1.752 1.752 0 003.75 20h16.5c.965 0 1.75-.785 1.75-1.75 0-.512-.223-.996-.621-1.337zM12 24a3.756 3.756 0 003.674-3H8.326A3.756 3.756 0 0012 24z"/></svg>
+      </button>
+    </div>
   </div>
 </template>
