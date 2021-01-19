9b6e214ef1915ea0b120ced3a90a6de27dc4ed47
Joshua Galit <joshuaimalay@gmail.com>
2021-01-19 12:47:39 +0800

    import TheHeader component and wrap nuxt-child in main tag

---
+++ pages/member.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TheHeader />
+    <main>
+      <nuxt-child />
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: '',
+      titleTemplate: '%s - Grace wealthness'
+    },
+    components: {
+      TheHeader: () => import('~/components/TheHeader')
+    }
+  }
+</script>
