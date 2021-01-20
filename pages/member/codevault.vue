0f2f15cfa0d9c1fbad7a986cc6b953d088e420aa
Joshua Galit <joshuaimalay@gmail.com>
2021-01-20 09:07:33 +0800

    create vue pages component

---
+++ pages/member/codevault.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-screen">
+    <div class="container m-auto max-w-lg rounded-lg shadow bg-white px-4 py-6">
+      <h1>this is the code vault</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Code Vault'
+    }
+  }
+</script>
---
+++ pages/member/invite.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-screen">
+    <div class="container m-auto max-w-lg rounded-lg shadow bg-white px-4 py-6">
+      <h1>this is the invite</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Invite Friends'
+    }
+  }
+</script>
