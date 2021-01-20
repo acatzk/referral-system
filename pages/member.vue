85ded1e149344597d3a5b62d09796dfe3e54a532
Joshua Galit <joshuaimalay@gmail.com>
2021-01-20 09:43:55 +0800

    add toggle boolean to trigger toggle hide and close event add nuxt-child for dynamic show content

--- pages/member.vue
+++ pages/member.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex min-h-screen h-screen">
-    <div class="hidden w-72 bg-white border-r border-gray-200 sm:block">
+    <div class="hidden w-72 bg-white border-r border-gray-200 sm:block" v-show="toggle">
       <TheSideNav />
     </div>
     <div class="flex-grow flex flex-col overflow-y-hidden">
-      <TheHeader />
+      <TheHeader>
+        <template #toggle-button>
+          <button @click.prevent="toggle = !toggle" class="focus:outline-none rounded focus:ring-2 ring-green-400 p-1">
+            <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
+            </svg>
+          </button>
+        </template>
+      </TheHeader>
       <div class="flex-grow overflow-y-auto">
         <nuxt-child />
       </div>
@@ -21,6 +29,11 @@
     components: {
       TheHeader: () => import('~/components/TheHeader'),
       TheSideNav: () => import('~/components/TheSideNav')
+    },
+    data () {
+      return {
+        toggle: true
+      }
     }
   }
 </script>
