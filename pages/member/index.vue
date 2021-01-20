9c02156df3ff52e8e7835d5ed31a972f28ee1044
Joshua Galit <joshuaimalay@gmail.com>
2021-01-20 09:45:01 +0800

    change dummy content

--- pages/member/index.vue
+++ pages/member/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <h1>this is the content</h1>
+  <div class="flex h-screen">
+    <div class="container m-auto max-w-lg rounded-lg shadow bg-white px-4 py-6">
+      <h1>this is a dashboard</h1>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    head: {
+      title: 'Dashboard'
+    }
+  }
+</script>
