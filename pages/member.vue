23eee57e42924f3a671d48f72bfcb2ce00c75153
Joshua Galit <joshuaimalay@gmail.com>
2021-02-01 17:50:37 +0800

    move AddSlotCard and import component

--- pages/member.vue
+++ pages/member.vue
@@ -14,19 +14,7 @@
         </template>
       </TheHeader>
       <div class="flex-grow overflow-y-auto px-4">
-        <div class="flex h-screen" v-if="!isValid">
-          <div class="container m-auto max-w-lg">
-            <div class="rounded-lg shadow bg-white px-4 py-6">
-              <div class="flex items-center flex-col space-y-3">
-                <h1>We have detected that you don't have a slot yet.</h1>
-                <button class="flex items-center py-2.5 px-3 bg-green-500 rounded-md focus:outline-none hover:opacity-80 focus:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition ease-in-out duration-150 space-x-1">
-                  <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-                  <span class="text-white text-sm font-medium uppercase">Add Slot</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <AddSlotCard v-if="!isValid"/>
         <nuxt-child v-else/>
       </div>
     </div>
@@ -41,7 +29,8 @@
     },
     components: {
       TheHeader: () => import('~/components/TheHeader'),
-      TheSideNav: () => import('~/components/TheSideNav')
+      TheSideNav: () => import('~/components/TheSideNav'),
+      AddSlotCard: () => import('~/components/AddSlotCard')
     },
     data () {
       return {
