1d264243494ee181f97ee330a248dfdbe7a23a34
Joshua Galit <joshuaimalay@gmail.com>
2021-02-03 22:20:04 +0800

    fix arrow function in checkRole params

--- server/utils/Auth.js
+++ server/utils/Auth.js
@@ -145,7 +145,7 @@ const serializeUser = user => {
 /**
  * @DESC Check role middlware
  */
-const checkRole = roles = (req, res, next) => 
+const checkRole = roles => (req, res, next) => 
   !roles.includes(req.user.role)
     ? res.status(401).json('Unauthorized')
     : next()
