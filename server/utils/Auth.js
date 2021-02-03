99b808cc38d446c10c72410e9c64ec4022e56507
Joshua Galit <joshuaimalay@gmail.com>
2021-02-03 19:59:44 +0800

    create login authentication role base check username and email validation with jwt token

--- server/utils/Auth.js
+++ server/utils/Auth.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('./../config')
 const User = require('./../models/User')
 
 /**
  * @DESC To register the user
  * (ADMIN, SUPER_ADMIN, USER) 
  */
-const userRegistration = async (userDetails, role, res) => {
+const registration = async (userDetails, role, res) => {
   try {
     // validate username
     let usernameNotTaken = await(validateUsername(userDetails.username))
@@ -44,7 +46,67 @@ const userRegistration = async (userDetails, role, res) => {
 
   } catch (err) {
     return res.status(500).json({
-      message: `Unable to create your account. \n ${err}`,
+      message: `Unable to create your account. ${err}`,
+      success: false
+    })
+  }
+}
+
+/**
+ * @DESC To login user 
+ * authentication role base
+ * (ADMIN, SUPER_ADMIN, USER) 
+ */
+const login = async (credentials, role, res) => {
+  let { username, password } = credentials
+  
+  // check if username is in the database
+  const user = await User.findOne({ username })
+  if (!user) {
+    return res.status(404).json({
+      message: `Username is not found. Invalid login credentials. `,
+      success: false
+    })
+  }
+
+  // check role login authentication
+  if (user.role !== role) {
+    return res.status(403).json({
+      message: `Please make sure you are logging in from the right portal.`,
+      success: false
+    })
+  }
+
+  // check for the password
+  let isMatch = await bcrypt.compare(password, user.password)
+  if (isMatch) {
+    // define payload for token
+    let payload = {
+      user_id: user._id,
+      role: user.role,
+      username: user.username,
+      email: user.email
+    }
+    // expiration
+    let expiration_date = { expiresIn: '7 days' }
+
+    // add paylood, secret and expiration date to get the jwt token
+    let token = jwt.sign(payload, SECRET, expiration_date)
+
+    let result = {
+      ...payload,
+      token: `Bearer ${token}`
+    }
+
+    return res.status(200).json({
+      ...result,
+      message: `Hurry! You are now logged in.`,
+      success: true
+    })
+
+  } else {
+    return res.status(403).json({
+      message: `Incorrect password.`,
       success: false
     })
   }
@@ -60,4 +122,4 @@ const validateEmail = async email => {
   return user ? false : true
 }
 
-module.exports = { userRegistration }
+module.exports = { login, registration }
