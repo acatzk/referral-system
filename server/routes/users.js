57aa9870d0b494b0aeda09740980f8b9918240e9
Joshua Galit <joshuaimalay@gmail.com>
2021-02-03 20:00:08 +0800

    import login and add post route base on role

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,34 @@
 const router = require('express').Router()
-const { userRegistration } = require('./../utils/Auth')
+const { login, registration } = require('./../utils/Auth')
 
 // User registration route
 router.post('/register-user', async (req, res) => {
-  await userRegistration(req.body, 'user', res)
+  await registration(req.body, 'user', res)
 })
 
 // Admin registration route
 router.post('/register-admin', async (req, res) => {
-  await userRegistration(req.body, 'admin', res)
+  await registration(req.body, 'admin', res)
 })
 
 // Super admin registration route
 router.post('/register-super-admin', async (req, res) => {
-  await userRegistration(req.body, 'superadmin', res)
+  await registration(req.body, 'superadmin', res)
+})
+
+// User login route
+router.post('/login-user', async (req, res) => {
+  await login(req.body, 'user', res)
+})
+
+// Admin login route
+router.post('/login-admin', async (req, res) => {
+  await login(req.body, 'admin', res)
+})
+
+// Superadmin login route
+router.post('/login-super-admin', async (req, res) => {
+  await login(req.body, 'superadmin', res)
 })
 
 module.exports = router
