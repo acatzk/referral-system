83ce01203681b1050b6524902094256b442760a5
Joshua Galit <joshuaimalay@gmail.com>
2021-02-04 12:06:39 +0800

    import ErrorIcon to show only if error ocured

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,7 @@
                         type="text" 
                         v-model="user.email"
                         :class="submitted && $v.user.email.$error ? 'is-invalid' : 'focus:ring-green-400'" />
-                <div v-if="submitted && $v.user.email.$error" class="absolute right-2 top-8 text-red-500 flex items-center">
-                  <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
-                </div>
+                <ErrorIcon v-if="submitted && $v.user.email.$error" />
               </div>
               <div v-if="submitted && $v.user.email.$error" class="invalid-feedback">
                 <span v-if="!$v.user.email.required">Email is required</span>
@@ -32,9 +30,7 @@
                         type="password" 
                         v-model="user.password"
                         :class="submitted && $v.user.password.$error ? 'is-invalid' : 'focus:ring-green-400'" />
-                <div v-if="submitted && $v.user.password.$error" class="absolute right-2 top-8 text-red-500 flex items-center">
-                  <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
-                </div>
+                <ErrorIcon v-if="submitted && $v.user.password.$error"  />
               </div>
               <div v-if="submitted && $v.user.password.$error" class="invalid-feedback">
                 <span v-if="!$v.user.password.required">Password is required</span>
@@ -66,6 +62,9 @@
   export default {
     head: {
       title: 'Login - Grace wealthness'
+    },
+    components: {
+      ErrorIcon: () => import('~/components/ErrorIcon')
     },
     data () {
       return {
