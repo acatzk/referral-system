7942d42d05066eee32ce4ddcbd3c1220bfbe51aa
Joshua Galit <joshuaimalay@gmail.com>
2021-02-04 11:32:08 +0800

    git rid of shadow-inner in input field style

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
                         placeholder="Enter Email" 
                         type="text" 
                         v-model="user.email"
-                        :class="submitted && $v.user.email.$error ? 'is-invalid' : 'shadow-inner focus:ring-green-400'" />
+                        :class="submitted && $v.user.email.$error ? 'is-invalid' : 'focus:ring-green-400'" />
                 <div v-if="submitted && $v.user.email.$error" class="absolute right-2 top-8 text-red-500 flex items-center">
                   <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
                 </div>
@@ -31,7 +31,7 @@
                         placeholder="Enter Password" 
                         type="password" 
                         v-model="user.password"
-                        :class="submitted && $v.user.password.$error ? 'is-invalid' : 'shadow-inner focus:ring-green-400'" />
+                        :class="submitted && $v.user.password.$error ? 'is-invalid' : 'focus:ring-green-400'" />
                 <div v-if="submitted && $v.user.password.$error" class="absolute right-2 top-8 text-red-500 flex items-center">
                   <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
                 </div>
