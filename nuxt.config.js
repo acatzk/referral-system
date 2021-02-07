94a6358797613c1d9e047628ca8b734e8512db8d
Joshua Galit <joshuaimalay@gmail.com>
2021-02-07 21:28:19 +0800

    remove imported googlefonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto&display=swap' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
